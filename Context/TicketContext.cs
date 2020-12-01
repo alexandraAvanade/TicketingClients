1c489a7e83717ea3566134092461df8a14c63517
alexandra.e.irimia <alexandra.e.irimia@WINAPBY2JK8NYD0>
2020-12-01 10:08:18 +0100

    Add project files.

---
+++ Context/TicketContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using TicketingClients.Model;
+
+namespace TicketingClients.Context
+{
+    public class TicketContext : DbContext
+    {
+        DbSet<Ticket> Ticket { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())  //forzo il config builder ad andare a cercare il file in esecuzione del programma
+                .AddJsonFile("appsetings.json")
+                .Build();
+            string connString = config.GetConnectionString("TicketDb");
+           // config.GetSection("ConnectionString")["TicketDb"]; alternativa
+            optionsBuilder.UseSqlServer("connString");
+        }
+        protected TicketContext()
+        {
+        }
+    }
+}
